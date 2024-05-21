cdcbe694e59acc3644f3d21eabd4a4774c308631
Valere <bill.carson@valrsoft.com>
2024-05-21 13:39:05 +0200

    Initial Commit

---
+++ SimplePosthogNSE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimplePosthogNSE
+//
+//  Created by valere on 21/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimplePosthogNSE/SimplePosthogNSEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePosthogNSEApp.swift
+//  SimplePosthogNSE
+//
+//  Created by valere on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimplePosthogNSEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
