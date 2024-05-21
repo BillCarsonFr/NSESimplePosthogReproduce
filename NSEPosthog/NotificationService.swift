e10123c85fdf52ed7ed96d7d0fc9f8c0a7177d49
Valere <bill.carson@valrsoft.com>
2024-05-21 14:54:17 +0200

    quick commit

---
+++ NSEPosthog/NotificationService.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationService.swift
+//  NSEPosthog
+//
+//  Created by valere on 21/05/2024.
+//
+
+import UserNotifications
+import PostHog
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    var posthog: PostHogSDK?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        posthog = PostHogSDK.shared
+        posthog?.setup(PostHogConfig(
+         apiKey: "FFF"
+        ))
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '15.0'
+
+target 'NSEPosthog' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NSEPosthog
+  pod 'PostHog', '~> 3.2.5'
+
+end
+
+target 'SimplePosthogNSE' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SimplePosthogNSE
+
+  target 'SimplePosthogNSETests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SimplePosthogNSEUITests' do
+    # Pods for testing
+  end
+
+end
